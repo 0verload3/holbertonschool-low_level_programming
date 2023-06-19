4682397aed5d28e9f6ac8d82f9f502f14065513e
0verload3 <wpaulgonzalez0201@gmail.com>
2023-06-19 01:56:31 +0000

    prnting something

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ *
+ * main- Entry point
+ * printf- Print string
+ * print- proper string
+ * Return-always 0 (success)
+ */
+int main(void)
+{
+int print = "with proper grammar, but the outcome is a piece of art,"
+printf ("%d", print)
+return (0)
+}
