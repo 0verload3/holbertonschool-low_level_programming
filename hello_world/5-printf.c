1f38ee3e05fc5b9167e3b5e7eaaa6632c45478a4
0verload3 <wpaulgonzalez0201@gmail.com>
2023-06-19 02:39:21 +0000

    print something...over999?

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /*
- * main- Entry point
- * printf- Print string
+ * main-Entry point
+ * printf-Print string
  * Return-always 0 (success)
- */
-int main(void)
+ */int main(void)
 {
 printf("With proper grammar, but the outcome is a piece of ar,");
 return (0);
