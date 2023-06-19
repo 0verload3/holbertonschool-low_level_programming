b7251360c9724615145b4b99923d798828b82c78
0verload3 <wpaulgonzalez0201@gmail.com>
2023-06-19 02:35:49 +0000

    printing somethin...999

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /*
- *
  * main- Entry point
  * printf- Print string
- * print- proper string
  * Return-always 0 (success)
  */
 int main(void)
 {
-int print = "with proper grammar, but the outcome is a piece of art,"
-printf ("%d", print)
-return (0)
-}
+printf("With proper grammar, but the outcome is a piece of ar,");
+return (0);
+};
