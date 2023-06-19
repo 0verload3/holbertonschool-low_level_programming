92efdc0a701422e3705b261683b5f1f8eff525a4
0verload3 <wpaulgonzalez0201@gmail.com>
2023-06-19 00:30:14 +0000

    puts.c.1

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,3 @@
 #include <stdio.h>
-/*
- * Printing
- * exact
- * string
-*/int main(void)
-{
-	int puts(Programming is like building a multilingual puzzle * s);
-	return (0);
-};
+/* Print exact string*/
+int main(void) { puts("Programming is like building a multilingual puzzle\n");return (0);}
