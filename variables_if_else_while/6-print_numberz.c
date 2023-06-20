fb6adf1e68f91d2d663f3412c0aa5639e633b1b0
0verload3 <wpaulgonzalez0201@gmail.com>
2023-06-20 23:42:31 +0000

    Printing numZzZ.1

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main- entry point
+ *Return: always 0
+ */
+int main(void)
+{
+	char base;
+
+	for (base = '0' ; base <= '9'; base++)
+		putchar (base);
+putchar('\n');
+return (0);
+}
