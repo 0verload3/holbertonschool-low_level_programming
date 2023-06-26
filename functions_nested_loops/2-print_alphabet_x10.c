80dd3a63c14ac5d91d391053c0bea5c37d414822
0verload3 <wpaulgonzalez0201@gmail.com>
2023-06-26 19:00:23 +0000

    Alphabetx10?.2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print abc x3
+ *
+*/
+void print_alphabet_x10(void)
+{
+	char alp;
+	char spa;
+
+	for (alp = 0;  alp <= 10; alp++) {
+		for (spa = 'a'; spa <= 'z'; spa++)
+		_putchar(spa);
+		_putchar('\n');
+	}
+_putchar('\n');
+}
+
