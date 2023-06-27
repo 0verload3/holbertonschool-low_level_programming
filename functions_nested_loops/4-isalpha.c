7d58adc3789691bf24d3440890e8c5bc670a099c
0verload3 <wpaulgonzalez0201@gmail.com>
2023-06-27 09:53:54 +0000

    ALPHA check.5

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -10,11 +10,6 @@ int _isalpha(int c)
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
-
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
@@ -23,6 +18,7 @@ int _isalpha(int c)
 	{
 		return (0);
 	}
+
 
 _putchar('\n');
 return (0);
