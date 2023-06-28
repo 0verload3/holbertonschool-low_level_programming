3c20ac6405c8745e57ce41e77b154ca6610cc578
0verload3 <wpaulgonzalez0201@gmail.com>
2023-06-28 15:21:35 +0000

    printing 98.3

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,18 +6,18 @@
  */
 void print_to_98(int n)
 {
-	while (n >= 98 || n <= 98)
+	while (n != 98)
 	{
-		if (n >= 98)
+		if (n > 98)
 		{
 			printf("%d, ", n);
 			(n--);
 		}
-		else
+		else if (n < 98)
 		{
 			printf("%d, ", n);
 			(n++);
 		}
 	}
-	printf("\n");
+	printf("98\n");
 }
