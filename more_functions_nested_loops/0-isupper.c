49ffa435555d199ad8467bf5f5ce35fa52b47844
0verload3 <wpaulgonzalez0201@gmail.com>
2023-06-28 18:10:16 +0000

    ASCII Upper check.1

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_isupper- verifies uppercase
+ *@c: Char to be checked
+ *Return: if-else
+ */
+int _isupper(int c)
+{
+	if (c <= 90)
+		return (1);
+	else
+		return (0);
+}
