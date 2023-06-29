5c5fd49b9f8fffc1851fc731387bdba9d87478e1
0verload3 <wpaulgonzalez0201@gmail.com>
2023-06-29 05:04:58 +0000

    diagonal like my life

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_line-  I feel like I am diagonally parked in a parallel universe.
+ *@n: number of \'s.
+ */
+void print_line(int n)
+{
+	int l;
+
+	for (l = 0; l <= 0; l++)
+		if (n > 0)
+		{
+			_putchar(92);
+			_putchar('\n');
+		}
+
+		else if (n <= 0)
+			_putchar('\n');
+}
