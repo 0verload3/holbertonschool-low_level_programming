66dae5934ad4603551b31fbd4872758eb86c5707
0verload3 <wpaulgonzalez0201@gmail.com>
2023-06-29 16:34:43 +0000

    printing terminal lines not coca-colas.4

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,15 @@ void print_line(int n)
 {
 	int l;
 
-	for (l = 0; l < n; l++)
-	{
-		_putchar(95);
-		_putchar('\n');
-	}
 	if (n <= 0)
 		_putchar('\n');
 
+	else
+
+		for (l = 0; l < n; l++)
+		{
+			_putchar(95);
+		}
+_putchar('\n');
 }
 
